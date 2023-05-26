0ac1a0bdd920d4e87e8725af9e2523ed1be596c0
Connor Jacobs <connorjacobs@Connors-MacBook-Pro-2.local>
2023-05-26 09:17:19 -0700

    this one easy

--- validPalindrom/Solution.py
+++ validPalindrom/Solution.py
@@ -2,16 +2,22 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alphaNum = re.sub(r'W\+', '', s)
+        alphaNum = re.sub(r'\W+', '', s)
         alphaNum = alphaNum.lower()
         print(alphaNum)
-            
-        for i in range(len(alphaNum)):
-            if(alphaNum[i] != alphaNum[len(alphaNum)-i]):
+        
+        if len(alphaNum) == 0:
+            return True
+
+        index = int(len(alphaNum) / 2)
+
+        for i in range(len(index)):
+            if alphaNum[i] == '_' or alphaNum[len(alphaNum) -1 -i] == '_':
+                continue;
+
+            if(alphaNum[i] != alphaNum[len(alphaNum) -1 -i]):
                 return False 
-            else:
-                return True
 
-solution = Solution()
+        return True
 
-print(solution.isPalindrome("racecar"))
+        
